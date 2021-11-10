e6dd0d1d1356b44140f71e640b1f791c1593524b
Katri <katri.rytkonen@outlook.com>
2021-11-10 10:52:59 +0200

    Viikko 2: Tehtävä 2: toml-tiedosto tulostuu oikein

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,5 +1,6 @@
 from urllib import request
 from project import Project
+import toml
 
 
 class ProjectReader:
@@ -9,7 +10,14 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+#        print(content)
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        kirjasto = toml.loads(content, _dict=dict)
+
+        name = kirjasto['tool']['poetry']['name']
+        description = kirjasto['tool']['poetry']['description']
+        dependencies = kirjasto['tool']['poetry']['dependencies'].keys()
+        devel_dependencies = kirjasto['tool']['poetry']['dev-dependencies']
+
+        return Project(name, description, dependencies, devel_dependencies)
