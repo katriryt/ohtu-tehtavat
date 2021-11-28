332be4abe321af4f98197b38bbdd77c6a669d78b
Katri <katri.rytkonen@outlook.com>
2021-11-28 13:38:25 +0200

    Viikko 4: Tehtävä 2: Testit luotu, koodi korjattu

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,21 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_kortille_ladataan_rahaa_jos_summa_on_positiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 10
+        pos_summa = 3
+
+        self.kassa.lataa(maksukortti_mock, pos_summa)
+
+        maksukortti_mock.lataa.assert_called_with(pos_summa)
+
+    def test_kortille_ei_ladata_rahaa_jos_summa_on_negatiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 10
+        neg_summa = -3
+
+        self.kassa.lataa(maksukortti_mock, neg_summa)
+
+        maksukortti_mock.lataa.assert_not_called()
