7d5171faf0e64de84725fba95fe2799cfa1fd3e2
Katri <katri.rytkonen@outlook.com>
2021-12-09 16:09:24 +0200

    Vko6: Tehtävät 2 ja 3: luokat All, Not, HawFewerThan ja Or toteutettu

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,21 +1,58 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+from matchers import And, HasAtLeast, PlaysIn, HasFewerThan, Not, Or, All
 
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
+    # Annettu alkuperäinen kysely
+#    matcher = And(
+#        HasAtLeast(5, "goals"),
+#        HasAtLeast(5, "assists"),
+#        PlaysIn("PHI")
+#    )
+
+# Tehtävä 2: testaa, että palauttaa Not, toimii
+#    matcher = And(
+#        Not(HasAtLeast(1, "goals")),
+#        PlaysIn("NYR")
+#    )
+
+    # tehtävä 2: testattu, että All toimii, OK
+#    matcher = All()
+
+#    matcher = And(
+#        HasAtLeast(1, "goals"),
+#        PlaysIn("NYR")
+#    )
+#
+#
+    # Tehtävä 2: testaa, että palauttaa FewerThan, toimii
+#    matcher = And(
+#        HasFewerThan(1, "goals"),
+#        PlaysIn("NYR")
+#    )
+
+    # Tehtävä 3: Testaa Or-luokan toimintaa. toimii.
+#    matcher = Or(
+#        HasAtLeast(30, "goals"),
+#        HasAtLeast(50, "assists")
+#    )
+
+    # Tehtävä 3: Testaa Or luokan toimintaa, toimii
     matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(5, "assists"),
-        PlaysIn("PHI")
+        HasAtLeast(40, "points"),
+        Or(
+            PlaysIn("NYR"),
+            PlaysIn("NYI"),
+            PlaysIn("BOS")
+        )
     )
 
     for player in stats.matches(matcher):
         print(player)
-
 
 if __name__ == "__main__":
     main()
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -9,6 +9,18 @@ class And:
         
         return True
 
+class Or: 
+    def __init__(self, *matchers): 
+        # Tähti indikoi vaihtuvan pituista listaa
+        self._matchers = matchers
+
+    def matches(self, player):
+        for matcher in self._matchers:
+            if matcher.matches(player):
+                return True
+
+        return False
+
 class PlaysIn:
     def __init__(self, team):
         self._team = team
@@ -25,3 +37,29 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+
+class All: 
+    def __init__(self):
+        pass
+
+    def matches(self, player):
+        return True
+
+class Not:
+    def __init__(self, resultmodel):
+        self._resultmodel = resultmodel
+
+    def matches(self, player):
+        if self._resultmodel.matches(player):
+            return False
+        else:
+            return True
+
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+    
+    def matches(self, player):
+        player_value = getattr(player, self._attr)
+        return player_value < self._value
