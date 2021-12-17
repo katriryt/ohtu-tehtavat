3871d7f7e96da8f1c3058b262064fe44e92ad048
Katri <katri.rytkonen@outlook.com>
2021-12-17 16:50:19 +0200

    Vko7, teht4: Koodi refaktoroitu (kommentit poistettu)

--- viikko7/kivi-paperi-sakset/src/tekoaly.py
+++ viikko7/kivi-paperi-sakset/src/tekoaly.py
@@ -1,6 +1,5 @@
 class Tekoaly:
     def __init__(self):
-#        print("luodaan tekoäly")
         self._siirto = 0
 
         self._siirrot = {
@@ -12,25 +11,7 @@ class Tekoaly:
         self._siirto = self._siirto + 1
         self._siirto = self._siirto % 3
 
-#        print(f"anna siirrossa: siirto on {self._siirto}")
-
         if self._siirto in self._siirrot: 
-#            print(f"palauttaa komentoa {self._siirrot[self._siirto]}")
             return self._siirrot[self._siirto]
         else:
-#            print("palauttaa s:ää")
             return "s"
-
-#        if self._siirto == 0:
-##            print("tietokone on nollan jakojäännöksessä")
-#            return "k"
-#        elif self._siirto == 1:
-##            print("tietokone on yhden jakojäännöksessä")
-#            return "p"
-#        else:
-##            print("tietokone on muussa jakojäännöksessä")
-#            return "s"
-
-#    def aseta_siirto(self, siirto):
-        # ei tehdä mitään
-#        pass
